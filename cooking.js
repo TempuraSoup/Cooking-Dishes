1707f62dca49e48fcfca709e88221c222c3eb45e
Crolwick <UIGiren@gmail.com>
2023-07-23 14:27:44 -0700

    commit

--- cooking.js
+++ cooking.js
@@ -92,26 +92,19 @@ async function randomButton()
     const data = await response.json();
     var randomNumber = Math.floor(Math.random() * data.number);
 
-    window.location.href="next.html";
-
     if (data.number > 0)
     {
         const randomDish = data.results[randomNumber];
 
         const summary = document.getElementById('dishSummary');
-        summary.innerHTML = `<h2>${title}</h2>
-        <p>${summary}</p>`;
+        summary.innerHTML = `<h2>${randomDish.title}</h2>
+        <p>${randomDish.summary}</p>`;
     }
     else
     {
         console.log('No dishes meet the specified parameters');
         alert('No dishes meet the specified parameters');
     }
-}
-
-function backButton()
-{
-    window.location.href="index.html";
 }
 
 changeCuisine();
